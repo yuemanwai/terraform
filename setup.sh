9679e6530617f7ad5e546e531e25cf30adb289e1
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2025-03-13 13:22:45 +0000

    新增 Google Cloud SDK 安裝步驟至 setup.sh 腳本

--- setup.sh
+++ setup.sh
@@ -32,6 +32,15 @@ kubectl version --client --output=yaml
 # 安裝 Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+# 安裝 Google Cloud SDK
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg -y
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list -y
+sudo apt-get update && sudo apt-get install google-cloud-cli
+gcloud init
+gcloud auth application-default login
+
 # 驗證安裝
 aws --version
 kubectl version
