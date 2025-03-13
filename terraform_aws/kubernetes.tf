a35444e4691e5edb3546a3d9a0c27c05c3d145fb
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2025-03-13 14:22:16 +0000

    移除 Nginx 部署和服務的 Kubernetes 清單，更新 Terraform 配置以反映變更

--- terraform_aws/kubernetes.tf
+++ terraform_aws/kubernetes.tf
@@ -10,16 +10,8 @@ provider "kubectl" {
   token                  = data.aws_eks_cluster_auth.demo.token
 }
 
-resource "kubernetes_manifest" "nginx_deployment" {
-  manifest = file("${path.module}/k8s_manifests/-deployment.yaml")
-}
-
 resource "kubernetes_manifest" "website_deployment" {
   manifest = file("${path.module}/k8s_manifests/website-deployment.yaml")
-}
-
-resource "kubernetes_manifest" "nginx_service" {
-  manifest = file("${path.module}/k8s_manifests/nginx-service.yaml")
 }
 
 resource "kubernetes_manifest" "website_service" {
