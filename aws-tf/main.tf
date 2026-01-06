ede8894cf2900e03e0d07ba32e28ac50752a1071
yuemanwai <yuemanwai@outlook.com>
2026-01-06 17:12:12 +0800

    Change instance type from t3.medium to t3.small for node group in EKS module

--- aws-tf/main.tf
+++ aws-tf/main.tf
@@ -69,22 +69,13 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 3
       desired_size = 2
     }
 
-    # two = {
-    #   name = "node-group-2"
-
-    #   instance_types = ["t3.small"]
-
-    #   min_size     = 1
-    #   max_size     = 2
-    #   desired_size = 1
-    # }
   }
 }
 
